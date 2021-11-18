ce66d8bb02a4119ca5f9f31d6fceae110a74d406
CKA <jwang252@ncsu.edu>
2021-11-17 23:28:17 -0800

    data page revision and create data exploration page

--- R-Shiny-Universal/server.R
+++ R-Shiny-Universal/server.R
@@ -13,8 +13,8 @@ data <- read_delim("anime_FinalInfo_from_Kitsu_API.csv",delim = " ")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output,session) {
-
-    output$Datatable <- renderTable({
+# data page 
+    output$Datatable1 <- renderDataTable({
         if(input$month == FALSE & input$age == FALSE){
             data %>% filter(year == input$year)
         }
@@ -27,10 +27,116 @@ shinyServer(function(input, output,session) {
         else if(input$month == TRUE & input$age == TRUE){
             data %>% filter(year == input$year & month == input$year_month & age_rating == input$age_rating)
         }
-        
-            
-        
+    })
+    output$downloadData <- downloadHandler(
+        filename = function() {
+            paste(input$year, ".csv", sep = "")
+        },
+        content = function(file){
+            write.csv(Datatable1, file, row.names = FALSE)
+        }
+    )
+    output$Datatable2 <- renderDataTable({
+        B <-data[order(data$rating,decreasing = TRUE),]
+        B %>% filter(Categories %in% input$genre|Genres %in% input$genre)%>% top_n(input$rank, rating)
         
     })
-
+    output$downloadData <- downloadHandler(
+        filename = function() {
+            paste(input$year, ".csv", sep = "")
+        },
+        content = function(file){
+            write.csv(Datatable1, file, row.names = FALSE)
+            
+        }
+    )
+    # data exploration page 
+    output$Datatable3 <- renderDataTable({
+        new <- na.omit(data)
+        if(input$percent1 == FALSE){
+            new %>% filter( year == input$year2) %>% summarize(mean_rating = mean(rating),
+                                                 mean_user = mean(user_count),
+                                                 mean_favorite = mean(favorite_count),
+                                                 mean_episode = mean(total_episode))
+        }
+        else{
+            new %>% group_by(year) %>%
+                summarize(percent = 100 * n() / nrow(new),mean_rating = mean(rating),
+                          mean_user = mean(user_count),
+                          mean_favorite = mean(favorite_count),
+                          mean_episode = mean(total_episode))
+        }
+    })
+    
+    output$Datatable4 <- renderDataTable({
+        new <- na.omit(data)
+        if(input$percent2 == FALSE){
+            new %>% filter(age_rating == input$age_rating2) %>% summarize(mean_rating = mean(rating),
+                                                 mean_user = mean(user_count),
+                                                 mean_favorite = mean(favorite_count),
+                                                 mean_episode = mean(total_episode))
+        }
+        else{
+            new %>% group_by(age_rating) %>%
+                summarize(percent = 100 * n() / nrow(new),mean_rating = mean(rating),
+                          mean_user = mean(user_count),
+                          mean_favorite = mean(favorite_count),
+                          mean_episode = mean(total_episode))
+        }
+    })
+    
+    output$plotyear <- renderPlot({
+        newdata <- data %>% filter(year == input$year3)
+        scatter <- ggplot(newdata, aes(x = user_count, y = rating))
+        scatter + geom_point(aes(color = age_rating)) + 
+            geom_smooth(method = "lm") + 
+            labs(title = paste0("user counts vs rating for year: ",input$year3), x = "user count", y = "anime rating") + 
+            scale_color_discrete(name = "age rating")
+    })
+    
+    output$plotmonth <- renderPlot({
+        newdata <- data %>% filter(month == input$month2)
+        bar <- ggplot(newdata, aes(x = year))
+        bar + geom_bar(aes(fill = as.factor(age_rating)), 
+                         position = "stack",
+                         show.legend = NA) +         labs(x = "Year") + 
+            scale_fill_discrete(name = "age rating") + 
+            theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
+            labs(title = "year by age_rating ")
+    })
+    
+    
+    
+    
+    
+    
+    
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- R-Shiny-Universal/ui.R
+++ R-Shiny-Universal/ui.R
@@ -11,7 +11,7 @@ library(shiny)
 library(tidyverse)
 library(shinyWidgets)
 
-data <- read_delim("anime_FinalInfo_from_Kitsu_API.csv",delim = " ")
+data <- read_delim("anime_FinalInfo_from_Kitsu_API.csv",delim =" ")
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(theme = "style.css",
                   div(style = "padding: 1px 0px; width: '100%'",
@@ -75,39 +75,111 @@ shinyUI(fluidPage(theme = "style.css",
                                                          label = "Age Rating",
                                                          choices = c("G", "PG", "R"),
                                                          selected = "G"
-                                             ))
-                                             
+                                             )),
+                                             downloadButton("downloadData", "Download")
                                      ),
                                      mainPanel(
-                                       tableOutput("Datatable")
+                                       dataTableOutput("Datatable1")
                                      )),
                                      tabPanel(
-                                         "Based on criterion"
-                                     ),
-                                     tabPanel(
-                                         "save the data set"
+                                         "Based on rank and genre",
+                                         sidebarPanel(
+                                           h3("Select the anime based on the rating rank"),
+                                           pickerInput(inputId = "rank",
+                                                       label = "Ranking",
+                                                       choices = c(1,10,20,30,40,50,60,70,80,90,100),
+                                                       selected = 1
+                                           ),
+                                            
+                                          
+                                         h3("Based on Genre"),
+                                           radioButtons(inputId = "genre",
+                                                        label = "Categories",
+                                                        choices = c("Fantasy", "Magic","Action", "Comedy", "School Life","Science Fiction","Romance","Horror",
+                                                                    "Adventure","Music","Drama","Slice of Life","Sports",'Historical',"Mystery"),
+                                                        selected = "Fantasy"
+                                         ),
+                                         downloadButton("downloadData2", "Download")
+                                         ),
+                                         mainPanel(
+                                             dataTableOutput("Datatable2")
+
+                                         )
+                                       
+                                     )
+                                           
                                      )
                                  )
-                             ),
+                             ,
                              tabPanel(
                                  "Data Exploration",
                                  tabsetPanel(
                                      type = "tabs",
                                      
                                  tabPanel(
-                                     "Table Summaries"
+                                     "Table Summaries",
+                                     h3("Get summary statistics based on year"),
+                                     sidebarPanel(
+                                       radioButtons(inputId = "year2",
+                                                   label = "Years",
+                                                   choices = c(2010:2022),
+                                                   selected = 2010),
+                                       checkboxInput("percent1", 
+                                                     label = tags$span(style="color: red;","see percentage of year with summary"), 
+                                                     value = FALSE, 
+                                                     width = NULL),
+                                       br(),
+                                       br(),
+                                       h3("Get summary statistics based on age rating"),
+                                       radioButtons(inputId = "age_rating2",
+                                                    label = "Age Rating",
+                                                    choices = c("G", "PG", "R"),
+                                                    selected = "G"),
+                                       checkboxInput("percent2", 
+                                                     label = tags$span(style="color: red;","see percentage of age_rating with summary"), 
+                                                     value = FALSE, 
+                                                     width = NULL)
+                                     ),
+                                     mainPanel(
+                                       dataTableOutput("Datatable3"),
+                                       br(),
+                                       br(),
+                                       
+                                       dataTableOutput("Datatable4")
+                                     )
                                  ),
                                  tabPanel(
-                                     "Visualizations"
+                                     "Visualizations",
+                                     sidebarPanel(
+                                       h3("Plot based on year"),
+                                       sliderInput("year3",
+                                                   "Years",
+                                                   min = 2010,
+                                                   max = 2022,
+                                                   step = 1,
+                                                   value = 2010),
+                                       sliderInput("month2",
+                                                   "Months",
+                                                   min = 1,
+                                                   max = 12,
+                                                   step =1,
+                                                   value = 1)
+                                         
+                                       ),
+                                     mainPanel(
+                                       plotOutput("plotyear"),
+                                       plotOutput("plotmonth")
+                                     )
+                                     )
                                  )
                                  )
-                             ),
+                            ,
                              tabPanel(
                                  "Modeling",
                                  tabsetPanel(
                                      type = "tabs",
                                      tabPanel(
-                                         "Modeling Info",
+                                         "Modeling Info"
                                      ),
                                      tabPanel(
                                          "Model Fitting"
